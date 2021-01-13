a41d556abb71e08afd1533769139f16bc6711b02
Szymon Gos <szymonngos@gmail.com>
2021-01-13 13:29:14 +0000

    remove formspree

--- javascript/sendEmail.js
+++ javascript/sendEmail.js
@@ -1,24 +1,12 @@
-// window.onload = function () {
-    
-//     const button = document.querySelector('#contact-form');
+const button = document.querySelector('#submit');
 
-//     button.addEventListener('submit', function (e) {
+button.addEventListener('click', function () {
 
-//         e.preventDefault();
-//         let name = document.querySelector('#guestName');
-//         let email = document.querySelector('#email');
-//         let message = document.querySelector('#message');
+    let name = document.querySelector('#guestName');
+    let email = document.querySelector('#email');
+    let message = document.querySelector('#message');
 
-//         let temporaryParams = {
-//             user_name: name.value,
-//             user_email: email.value,
-//             message: message.value
-//         };
-
-//         emailjs.send('service_hspoq4m', 'template_bx8ki79', temporaryParams);
-
-//        name.value = '';
-//        email.value = '';
-//        message.value = '';
-//     });
-// };
+    name.value = '';
+    email.value = '';
+    message.value = '';
+});
