be662e46e14b29e842318a2ea0e0c6fe3b99c3c0
Szymon Gos <szymonngos@gmail.com>
2021-01-13 12:05:24 +0000

    add kwes form cdn

--- javascript/sendEmail.js
+++ javascript/sendEmail.js
@@ -1,24 +1,24 @@
-window.onload = function () {
+// window.onload = function () {
     
-    const button = document.querySelector('#contact-form');
+//     const button = document.querySelector('#contact-form');
 
-    button.addEventListener('submit', function (e) {
+//     button.addEventListener('submit', function (e) {
 
-        e.preventDefault();
-        let name = document.querySelector('#guestName');
-        let email = document.querySelector('#email');
-        let message = document.querySelector('#message');
+//         e.preventDefault();
+//         let name = document.querySelector('#guestName');
+//         let email = document.querySelector('#email');
+//         let message = document.querySelector('#message');
 
-        let temporaryParams = {
-            user_name: name.value,
-            user_email: email.value,
-            message: message.value
-        };
+//         let temporaryParams = {
+//             user_name: name.value,
+//             user_email: email.value,
+//             message: message.value
+//         };
 
-        emailjs.send('service_hspoq4m', 'template_bx8ki79', temporaryParams);
+//         emailjs.send('service_hspoq4m', 'template_bx8ki79', temporaryParams);
 
-       name.value = '';
-       email.value = '';
-       message.value = '';
-    });
-};
+//        name.value = '';
+//        email.value = '';
+//        message.value = '';
+//     });
+// };
